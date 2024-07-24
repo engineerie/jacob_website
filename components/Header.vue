cc120e61c1856dbcd995aaae06b5d5fdf11fbbb5
Jacobb <jacob.bertilsson@gmail.com>
2024-07-24 19:50:31 +0100

    Added upcoming info

--- components/Header.vue
+++ components/Header.vue
@@ -1,35 +1,48 @@
 <template>
-    <div class="fixed w-full h-10 dark:bg-neutral-900 bg-neutral-100"> 
-<Breadcrumb class="lg:ml-60 -left-2 top-2" />
-        <div class="fixed flex right-2 top-2">
-            <UButton
-              :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
-              color="gray"
-              variant="ghost"
-              aria-label="Theme"
-              @click="isDark = !isDark"
-            />
-            <UButton
-              color="gray"
-              variant="ghost"
-              aria-label="github"
-              to = "https://github.com/engineerie"
-              target="empty"
-              class="px-2"
-            > <UIcon name="i-simple-icons-github" class="w-5 h-5" dynamic/> </UButton> 
-        </div>
-        
-        </div>
+  <div :class="headerClass">
+    <Breadcrumb class="lg:ml-60 -left-2 top-2" />
+    <div class="fixed flex right-2 top-2">
+      <UButton
+        :icon="
+          isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
+        "
+        color="gray"
+        variant="ghost"
+        aria-label="Theme"
+        @click="isDark = !isDark"
+      />
+      <UButton
+        color="gray"
+        variant="ghost"
+        aria-label="github"
+        to="https://github.com/engineerie"
+        target="empty"
+        class="px-2"
+      >
+        <UIcon name="i-simple-icons-github" class="w-5 h-5" dynamic />
+      </UButton>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-  const colorMode = useColorMode()
-  const isDark = computed({
-    get () {
-      return colorMode.value === 'dark'
-    },
-    set () {
-      colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-    }
-  })
-  </script>
+import { useRoute } from "vue-router";
+
+const colorMode = useColorMode();
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set() {
+    colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+  },
+});
+
+const route = useRoute();
+
+const headerClass = computed(() => {
+  return `fixed w-full h-10 ${
+    route.path === "/" ? "bg-transparent" : "dark:bg-neutral-900 bg-neutral-100"
+  }`;
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,101 +1,153 @@
 <template>
-  <div class="dark:opacity-60">
-    <!-- Apply a glitch effect during the out-in transition -->
-    <!-- Added Tailwind classes for responsive height control -->
-    <div class="sm:m-24 m-2">
-      <transition name="glitch" mode="out-in">
-        <video
-          width="100%"
-          height="auto"
-          autoplay
-          muted
-          playsinline
-          @ended="playNextVideo"
-          preload="auto"
-          class="video rounded-xl overflow-hidden"
-          :key="currentVideo"
-        >
-          <source :src="currentVideo" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </transition>
-      <transition
-        name="glitch"
-        mode="out-in"
-        @before-enter="beforeEnter"
-        @after-enter="afterEnter"
+  <div v-if="isContentLoaded">
+    <UModal v-model="isOpen" :overlay="true" class="sm:-mt-24">
+      <UCard
+        :ui="{
+          ring: '',
+          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+          rounded: 'rounded-xl',
+        }"
       >
-        <p class="text-sm mt-2">{{ currentDescription }}</p>
-        <!-- Description text below the video -->
-      </transition>
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3
+              class="text-base font-semibold leading-6 text-gray-900 dark:text-white"
+            >
+              Upcoming
+            </h3>
+            <UButton
+              color="gray"
+              variant="ghost"
+              icon="i-heroicons-x-mark-20-solid"
+              class="-my-1"
+              @click="toggleModal"
+            />
+          </div>
+        </template>
+        <ul class="list-none">
+          <li>
+            <NuxtLink
+              to="https://research-architecture.org/Book-Series"
+              target="blank"
+              class="text-blue-600 dark:text-blue-500"
+              >Article in CRA III: Common Sensing (2024)
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              to="https://www.mustarinda.fi/en/magazine"
+              target="blank"
+              class="text-blue-600 dark:text-blue-500"
+              >Article with George Ridgway in Mustarinda Magazine (2024)
+            </NuxtLink>
+          </li>
+        </ul>
+      </UCard>
+    </UModal>
+    <div class="dark:opacity-40 sm:m-44">
+      <div :class="{ gradient: isGradientActive }" @click="toggleGradient">
+        <UCard :ui="{ rounded: 'rounded-xl' }">
+          <transition name="glitch" mode="out-in">
+            <video
+              width="100%"
+              height="auto"
+              autoplay
+              muted
+              playsinline
+              @ended="playNextVideo"
+              preload="auto"
+              class="video rounded-xl overflow-hidden shadow-lg"
+              :key="currentVideo"
+            >
+              <source :src="currentVideo" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </transition>
+          <transition name="glitch" mode="out-in">
+            <p class="text-sm mt-2">{{ currentDescription }}</p>
+          </transition>
+        </UCard>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 // Define the array of videos with descriptions
 const videos = [
   {
     src: "/videos/9.mp4",
-    description: "A serene view of the mountains during sunrise.",
+    description: "",
   },
   {
     src: "/videos/2.mp4",
-    description: "An exciting downtown night scene with vibrant lights.",
+    description: "",
   },
   {
     src: "/videos/3.mp4",
-    description: "A peaceful beach with gentle waves lapping the shore.",
+    description: "",
   },
   {
     src: "/videos/4.mp4",
-    description: "A bustling city street scene captured at noon.",
+    description: "",
   },
   {
     src: "/videos/5.mp4",
-    description: "A quiet lake surrounded by forests in the early morning.",
+    description: "",
   },
   {
     src: "/videos/6.mp4",
-    description: "An artistic interpretation of urban architecture.",
+    description: "",
   },
   {
     src: "/videos/7.mp4",
-    description: "Night sky filled with stars over a calm desert.",
+    description: "",
   },
   {
     src: "/videos/8.mp4",
-    description: "Early morning dew on wildflowers in a meadow.",
+    description: "",
   },
   {
     src: "/videos/1.mp4",
-    description: "A fast-paced montage of various cityscapes.",
+    description: "",
   },
 ];
 
-// Function to shuffle an array
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
 
-// Shuffle videos initially
 shuffle(videos);
-
-// Reactive state for current video and description
 const currentVideoIndex = ref(0);
-const currentVideo = ref(videos[0].src); // Start with the first video source after shuffle
-const currentDescription = ref(videos[0].description); // Start with the first description
+const currentVideo = ref(videos[0].src);
+const currentDescription = ref(videos[0].description);
 
-// Function to play the next video
+const isOpen = ref(true); // Modal is open by default on each visit
+const isGradientActive = ref(isOpen.value); // Sync gradient with modal visibility
+const isContentLoaded = ref(false); // Controls the rendering of the main content
+
+onMounted(() => {
+  isContentLoaded.value = true; // Enable content rendering after component is mounted
+});
+
+// Watch isOpen to control the gradient activation
+watch(isOpen, (newVal) => {
+  isGradientActive.value = newVal;
+});
+
+function toggleModal() {
+  isOpen.value = !isOpen.value;
+}
+
 function playNextVideo() {
   currentVideoIndex.value = (currentVideoIndex.value + 1) % videos.length;
   currentVideo.value = videos[currentVideoIndex.value].src;
-  currentDescription.value = videos[currentVideoIndex.value].description; // Update current description based on index
+  currentDescription.value = videos[currentVideoIndex.value].description;
 }
 </script>
 
@@ -149,5 +201,49 @@ function playNextVideo() {
 .glitch-enter-to,
 .glitch-leave-from {
   opacity: 1;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.gradient {
+  --size: 750px;
+  --speed: 50s;
+  --easing: cubic-bezier(0.8, 0.2, 0.2, 0.8);
+
+  width: var(--size);
+  height: var(--size);
+  filter: blur(calc(var(--size) / 5));
+  background-image: linear-gradient(hsl(120, 1%, 41%), hsl(0, 0%, 60%)),
+    url("/svg/nnnoise.svg");
+  animation: rotate var(--speed) var(--easing) alternate infinite;
+  border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
+}
+
+@media (min-width: 720px) {
+  .gradient {
+    --size: 200px;
+  }
+}
+
+body {
+  background-color: #071c39;
+  position: absolute;
+  inset: 0;
+  display: flex;
+  place-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+/* This is just to transition when you change the viewport size. */
+* {
+  transition: all 0.5s ease-out;
 }
 </style>
